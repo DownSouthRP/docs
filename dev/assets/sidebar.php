41f74eb5edd103b8d5629028ed02f09c5bd02ab2
andybrown833 <andersonbrown833@gmail.com>
2020-10-13 21:14:59 -0500

    0.7.3

--- dev/assets/sidebar.php
+++ dev/assets/sidebar.php
@@ -12,7 +12,15 @@
 
     <?php // ADD NEW ITEMS AT THE TOP OF THE LIST
 
-        echo '<a href="/dev/release/0.0.7.0/" class="list-group-item list-group-item-action">0.0.7.0 - LIVE
+        echo '<a href="/dev/release/0.7.3/" class="list-group-item list-group-item-action">0.7.3 - LIVE
+        <br>
+        <span class="focus-right badge badge-success">3 Additions</span>  
+        <span class="focus-right badge badge-warning">1 Updates</span> 
+        <span class="focus-right badge badge-danger">0 Removals</span> 
+        <span class="focus-right badge badge-info">0 Tips</span>
+        </a>';
+
+        echo '<a href="/dev/release/0.0.7.0/" class="list-group-item list-group-item-action">0.0.7.0
         <br>
         <span class="focus-right badge badge-success">1 Addition</span>  
         <span class="focus-right badge badge-warning">3 Updates</span> 
